f456efc4d07b9c63e82f92b8a3cd77ae0e4eb77d
Michael <mikeybsaccount@gmail.com>
2016-10-08 00:56:53 +0100

    Autoloading

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Neutron\\' => array($baseDir . '/core'),
+    'App\\' => array($baseDir . '/app'),
 );
