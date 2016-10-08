bd527c9ad755417f7302b70e85db9e0ad852fca4
Michael <mikeybsaccount@gmail.com>
2016-10-08 02:20:41 +0100

    sensible default

--- core/View.php
+++ core/View.php
@@ -4,7 +4,7 @@ namespace Neutron;
 
 class View {
 
-	public static function get($name, $data)
+	public static function get($name, $data = [])
 	{
 		extract($data);
 		return require APP_DIR . '/views/' . $name . '.tpl.php';
