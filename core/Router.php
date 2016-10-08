8b1066124fb94da31f0532905cfef4a5f4c49f3e
Michael <mikeybsaccount@gmail.com>
2016-10-08 02:08:42 +0100

    better error handling

--- core/Router.php
+++ core/Router.php
@@ -25,9 +25,23 @@ class Router {
 		if ( array_key_exists($uri, $this->routes) )
 		{
 			$route = explode('@', $this->routes[$uri]);
-			$class = new $route[0];
-			$method = $route[1];
-			return $class->$method();
+			if ( class_exists( $route[0] ) )
+			{
+				$class = new $route[0];
+				$method = $route[1];
+				if ( method_exists($class, $method) )
+				{ 
+					return $class->$method();
+				}
+				else
+				{
+					throw new Exception($route[0] . '::' . $route[1] . ' not found');
+				}
+			}
+			else
+			{
+				throw new Exception($route[0] . ' not found');
+			}
 		}
 
 		throw new Exception('Route of '. $uri .' not found');
