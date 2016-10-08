1e7f9b431bdfe1bc2e1438f7d4fb9a76b0388028
Michael <mikeybsaccount@gmail.com>
2016-10-08 01:38:01 +0100

    Add Request Object

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,4 @@ ini_set('display_errors', 'On');
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$basename = basename(dirname($_SERVER['PHP_SELF']));
-$request = str_replace($basename, '', $_SERVER['REQUEST_URI']);
-$request = str_replace('//', '/', $request);
-
-Neutron\Router::load(__DIR__ . '/app/routes.php')->route(rtrim($request, '/'));
+Neutron\Router::load(__DIR__ . '/app/routes.php')->route(Neutron\Request::uri());
